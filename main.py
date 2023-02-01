ce61fd33db8e1c4a4873bbfb2d5b2c7e9a2b7690
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2023-01-31 20:19:21 -0500

    +Import QResources

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from easy_mvp.application_manager import ApplicationManager
 from easy_mvp.intent import Intent
 from presenter.main import MainPresenter
-
+import view.ui.resources
+import view.ui.icons_rc
 
 app = ApplicationManager()
 initial_intent = Intent(MainPresenter)
