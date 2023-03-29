53b86754699e06ab836ca743b25503b11b645170
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2023-03-29 15:34:30 -0400

    The app data will be saved on <path_to_user_dir>/.blue-nauta/data.db
    	It was tested on Windows and Linux

--- model/database/util.py
+++ model/database/util.py
@@ -1,7 +1,17 @@
 from sqlalchemy import create_engine
 from ..entity.entity import Base
+from pathlib import Path
+import os
 
-DB_URL = 'sqlite:///data.db?check_same_thread=False'
+
+__BLUE_NAUTA_FOLDER_PATH = Path(os.path.join(str(Path.home()), '.blue-nauta/'))
+
+if not __BLUE_NAUTA_FOLDER_PATH.exists():
+    __BLUE_NAUTA_FOLDER_PATH.mkdir()
+
+__BLUE_NAUTA_DB_PATH = str(os.path.join(str(__BLUE_NAUTA_FOLDER_PATH), 'data.db'))
+
+DB_URL = f'sqlite:///{__BLUE_NAUTA_DB_PATH}?check_same_thread=False'
 
 def create_database():
     engine = create_engine(DB_URL, echo=False, future=True)
