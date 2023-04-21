9621c71970315f8e034be4f4c845f9784f86c6b6
Rolando Rio <rolandorio06@gmail.com>
2023-04-21 16:58:13 -0400

    Style: Added app name and window icon

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from easy_mvp.application_manager import ApplicationManager
 from easy_mvp.intent import Intent
 from presenter.main import MainPresenter
+from util.resources_path import resource_path
 
-
-app = ApplicationManager()
+app = ApplicationManager(
+    app_name='Blue Nauta',
+    window_icon_path=resource_path('favicon.ico'),
+)
 initial_intent = Intent(MainPresenter)
 app.execute_app(initial_intent)
